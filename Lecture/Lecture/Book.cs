c13788a65fca3759c24bcac186b2ca3be23d05fe
s-parksu01 <s-parksu01@51L104-09>
2017-05-12 09:18:47 -0700

    xc

---
+++ Lecture/Lecture/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Lecture.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string CoverImage { get; set; }
+    }
+}
---
+++ Lecture/Lecture/BookManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Lecture.Models
+{
+    public class BookManager
+    {
+        public static List<Book> getBooks()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book { BookId = 1, Title = "Vulpate", Author = "Futurm", CoverImage = "Assets/1.png" });
+            books.Add(new Book { BookId = 2, Title = "Mazim", Author = "Sequiter Que", CoverImage = "Assets/2.png" });
+            books.Add(new Book { BookId = 3, Title = "Elit", Author = "Tempor", CoverImage = "Assets/3.png" });
+            return books;
+        }
+    }
+}
